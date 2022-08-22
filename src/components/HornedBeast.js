346d11efadbd694a783064a946cde6399a24ebb4
dee-desu <omardarweesh97@outlook.com>
2022-08-22 08:54:46 +0300

    react practice task

---
+++ src/components/HornedBeast.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class HornedBeast extends React.Component {
+    render() {
+        return(
+            <div className="hedoro-card">
+                <h2 className="horned-title">{this.props.title}</h2>
+                <img className="horned-pic" src={this.props.img} alt={this.props.title} title={this.props.title} />
+                <p className="beast-description">{this.props.description}</p>
+            </div>
+        )
+    }
+}
+
+export default HornedBeast;
