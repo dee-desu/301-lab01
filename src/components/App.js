3a7de1d38796365dc878bc72caba70ebe9b1ce83
dee-desu <omardarweesh97@outlook.com>
2022-08-24 09:21:00 +0300

    lab03

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,45 @@
 import React from "react";
 import Header from './Header';
 import Footer from './Footer';
-import Main from './Main'
+import Main from './Main';
+import data from "./Data";
+import SelectedBeast from "./selectedBeast";
 
 class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            show : false
+        }
+    }
+
+    showModal = () => {
+        this.setState({
+            show: !(this.state.show)
+        })
+    }
+
+
+    modalGetData = (key) => key;
+
     render() {
         return(
             <div>
-            <Header />
-            <div className="card">   
-            <Main />
-            </div>
-            <Footer />
+                <Header />
+                <div className="card">   
+                    <Main 
+                        data={data}
+                        showModalCard={this.showModal}
+                        toGetData={this.modalGetData}
+                    />
+                    <SelectedBeast 
+                        show={this.state.show} 
+                        hideCard={this.showModal}
+                     
+                    />
+                </div>
+                <Footer />
             </div>
         )
     }
